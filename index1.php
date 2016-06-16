8c0e93dbd68a1430ec06dd02f6f93d93f7fa12e8
jasonson82668 <jasonson82668@gmail.com>
2016-06-16 12:07:57 +0800

    Add files via upload

--- index1.php
+++ index1.php
@@ -32,6 +32,18 @@ class  PostOffice{
 
 
     function  splitroad(){
+    	$NewString = "高雄市楠梓區/高雄大學路/700號";
+    	$NewString1= "高雄市苓雅區/自強三路/15巷18號3樓";
+    	$NewString2= "高雄市前鎮區/鎮中路/800號之1";
+    	$NewString3= "新北市樹林區/路中路/280巷20號";
+    	
+$NewString = split ('[/.-]', $NewString);
+echo $NewString[1].'<br>';
+echo $NewString1[1].'<br>';
+echo $NewString2[1].'<br>';;
+echo $NewString3[1].'<br>';;
+
+
     	
     }
 
@@ -39,3 +51,4 @@ class  PostOffice{
 
 
 }
+?>
